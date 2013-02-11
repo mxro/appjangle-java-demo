921a03954e712c2c6404ec9ed5428bcc0e63a502
committer <com@dev>
2013-02-11 14:56:48 +1300

    implemented first basic version for caluclation of user names

--- appjangle-java-demo/src/main/java/com/appjangle/demos/appjanglejavademo/Calculations.java
+++ appjangle-java-demo/src/main/java/com/appjangle/demos/appjanglejavademo/Calculations.java
@@ -15,27 +15,34 @@ import java.util.Map;
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  */
 public class Calculations {
-    
+
     Types t;
     Session session;
-    
-    public Map<String, String> calculatePostsPerUser(Node posts) {
-        Map<String, String> res = new HashMap<String, String>();
-        
-        NodeList allPosts = posts.selectAll(t.aPost()).get();
-        
-        for (Node post : allPosts) {
-            
-        }
-        
+
+    public Map<String, Integer> calculatePostsPerUser(Node posts) {
+        Map<String, Integer> res = new HashMap<String, Integer>();
+
+        NodeList allUserNames = posts.selectAll(t.aPost())
+                                     .selectAll(t.aUserName()).get();
+
+        for (Node userName : allUserNames) {
+            incrementUserPostCount(userName.value(String.class), res);
+        }     
+
         return res;
     }
 
+    private void incrementUserPostCount(String userName, Map<String, Integer> counts) {
+        if (!counts.containsKey(userName)) {
+            counts.put(userName, 1);
+            return;
+        }
+        
+        counts.put(userName, counts.get(userName) +1);
+    }
+    
     public Calculations(Session session) {
         this.session = session;
         this.t = new Types(session);
     }
-    
-    
-    
 }
